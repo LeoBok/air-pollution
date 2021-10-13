9c43514adaa58dba786bc8f79474719da59cc4eb
Leonardo <leonardoboccali19@gmail.com>
2021-10-13 11:17:14 +0200

    installed css loader and configured in the webpacl.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,18 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+        ],
+    },
 
     plugins: [
         new HtmlWebpackPlugin({
