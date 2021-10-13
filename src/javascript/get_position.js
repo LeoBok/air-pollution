5a9dc204633fc200d2b14b77cc6771a84b6b477e
Leonardo <leonardoboccali19@gmail.com>
2021-10-13 21:23:15 +0200

    created a getPosition func which by clicking the btn,  will call the success func, if browser suport geolocation, or will call the error func

--- src/javascript/get_position.js
+++ src/javascript/get_position.js
@@ -1,1 +1,10 @@
 import { container, infoContainer, errParagraph, latitudeInput, longitudeInput, btn, apiKey, cityName, temperature, airQuality, AQIcolor, airborneParticulate25, cityNameValue, temperatureValue, pm25Value } from "./dom.js";
+
+
+function getPosition() {
+    btn.addEventListener('click', ()=>{
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+    });
+}
