6ad741efd57a8e374fa627b8834d2ff6e8c6e0fe
Leonardo <leonardoboccali19@gmail.com>
2021-10-13 13:55:47 +0200

    get the main container in a container variable, give a title to the app, const container = document.querySelector('.container');

--- src/javascript/dom.js
+++ src/javascript/dom.js
@@ -1,1 +1,19 @@
 const container = document.querySelector('.container');
+
+const title = document.createElement('h1');
+title.innerText = 'Check your Air Pollution:';
+container.appendChild(title);
+
+const coordInputContainer = document.querySelector('.coord-input-container');
+coordInputContainer.classList.add('.coord-input-container');
+container.appendChild(coordInputContainer);
+
+const latitudeInput = document.createElement('input');
+latitudeInput.classList.add('form-control');
+latitudeInput.placeholder = 'Your current latitude';
+coordInputContainer.appendChild(latitudeInput);
+
+const longitudeInput = document.createElement('input');
+longitudeInput.classList.add('form-control');
+longitudeInput.placeholder = 'Your current longitude';
+coordInputContainer.appendChild(longitudeInput);
