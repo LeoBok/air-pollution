18a26a0af3e645562f385461253d60d9b47a89da
Leonardo <leonardoboccali19@gmail.com>
2021-10-14 16:43:42 +0200

    exported the getCityAQI func and imported in the index.js file as searchCity func

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,6 @@
-import { style } from '../css/style.css'
-
-import { getPosition as currentPosition } from './get_position'
+import { style } from '../css/style.css';
+import { getPosition as currentPosition } from './get_position';
+import { getCityAQI as searchCity } from "./search_city.js";
 
 currentPosition();
+searchCity();
--- src/javascript/search_city.js
+++ src/javascript/search_city.js
@@ -1,5 +1,5 @@
 import { container, infoContainer, errParagraph, cityInput, apiKey, cityName, temperature, airQuality, AQIcolor, airborneParticulate25, cityNameValue, temperatureValue, pm25Value } from "./dom.js";
-
+export { getCityAQI }
 
 function getCityAQI() {
 
