cbb39ccdbd2fcfb2fa4c0665795965cbae4ee571
Leonardo <leonardoboccali19@gmail.com>
2021-10-14 16:17:46 +0200

    created getCityAQI func which will show the air quality of the city you want.this func will call the requestApi func and pass as parameter the city written in the input

--- src/javascript/search_city.js
+++ src/javascript/search_city.js
@@ -1,1 +1,13 @@
 import { container, infoContainer, errParagraph, cityInput, apiKey, cityName, temperature, airQuality, AQIcolor, airborneParticulate25, cityNameValue, temperatureValue, pm25Value } from "./dom.js";
+
+
+function getCityAQI() {
+
+    cityInput.addEventListener('keyup', e => {
+
+        if (e.key == 'Enter' && cityInput.value != '') {
+            requestApi(cityInput.value);
+        }
+        
+    })
+}
