f93509b51227b26354ad98ce71ddb4905f47021f
Leonardo <leonardoboccali19@gmail.com>
2021-10-14 15:48:57 +0200

    selected the imported dom elements and appended them the fetched value. also created an if\else statement for given air quality color to every air quality value

--- src/javascript/get_position.js
+++ src/javascript/get_position.js
@@ -41,6 +41,75 @@ function success(pos) {
     getCityData()
 
     async function getCityData() {
+
+        const response = await fetch(`https://api.waqi.info/feed/geo:${latitudeInput.value};${longitudeInput.value}/?token=${apiKey}`);
+        const jsonData = await response.json();
+
+        const city = jsonData.data.city.name;
+        const aqi = jsonData.data.aqi;
+        const temp = jsonData.data.iaqi.t.v;
+        const pm25 = jsonData.data.iaqi.pm25.v;
+
+        cityName.innerText = 'Your location is: ';
+        cityNameValue.innerText = `${city}`;
+        cityName.appendChild(cityNameValue);
+        infoContainer.appendChild(cityName);
         
+        airQuality.innerText = 'Air Quality: ';
+        airQuality.appendChild(AQIcolor);
+        infoContainer.appendChild(airQuality);
+
+        if (aqi <= 50) {
+
+            AQIcolor.classList.remove('aqi-color-moderate', 'aqi-color-unhealthy-for-sensitive-groups', 'aqi-color-unhealthy', 'aqi-color-very-unhealthy', 'aqi-color-hazardous')
+            AQIcolor.classList.add('aqi-color-good');
+    
+            AQIcolor.innerText = `${aqi}. Air quality is considered satisfactory 👍🏼`;
+        }
+        else if (aqi > 50 && aqi <= 100) {
+            
+            AQIcolor.classList.remove('aqi-color-good', 'aqi-color-unhealthy-for-sensitive-groups', 'aqi-color-unhealthy', 'aqi-color-very-unhealthy', 'aqi-color-hazardous')
+            AQIcolor.classList.add('aqi-color-moderate');
+
+            AQIcolor.innerText = `${aqi}. Air quality is acceptable 👌🏼`;
+        }
+        else if (aqi > 100 && aqi <= 150) {
+
+            AQIcolor.classList.remove('aqi-color-good', 'aqi-color-moderate', 'aqi-color-unhealthy', 'aqi-color-very-unhealthy', 'aqi-color-hazardous')
+            AQIcolor.classList.add('aqi-color-unhealthy-for-sensitive-groups');
+    
+            AQIcolor.innerText = `${aqis}. Members of sensitive groups may experience health effects. 👎🏼`;
+        }
+        else if (aqi > 150 && aqi <= 200) {
+
+            AQIcolor.classList.remove('aqi-color-good', 'aqi-color-moderate', 'aqi-color-unhealthy-for-sensitive-groups', 'aqi-color-very-unhealthy', 'aqi-color-hazardous')
+            AQIcolor.classList.add('aqi-color-unhealthy');
+    
+            AQIcolor.innerText = `${aqis}. Everyone may begin to experience health effects. 👎🏼`; 
+        }
+        else if (aqi > 200 && aqi <= 300) {
+
+            AQIcolor.classList.remove('aqi-color-good', 'aqi-color-moderate', 'aqi-color-unhealthy-for-sensitive-groups', 'aqi-color-unhealthy', 'aqi-color-hazardous')
+            AQIcolor.classList.add('aqi-color-very-unhealthy'); 
+    
+            AQIcolor.innerText = `${aqi}. Health warnings of emergency conditions. 👎🏼`;
+        }
+        else if (aqi > 300) {
+
+            AQIcolor.classList.remove('aqi-color-good', 'aqi-color-moderate', 'aqi-color-unhealthy-for-sensitive-groups', 'aqi-color-unhealthy', 'aqi-color-very-unhealthy')
+            AQIcolor.classList.add('aqi-color-hazardous');
+    
+            AQIcolor.innerText = `${aqi}. Health alert: everyone may experience more serious health effects. 👎🏼`;
+        }
+
+        airborneParticulate25.innerText = 'Airborne particulate matter(PM2.5): ';
+        pm25Value.innerText = `${pm25}`;
+        airborneParticulate25.appendChild(pm25Value);
+        infoContainer.appendChild(airborneParticulate25);
+
+        temperature.innerText = 'Temperature: ';
+        temperatureValue.innerText = `${temp} °C`;
+        temperature.appendChild(temperatureValue)
+        infoContainer.appendChild(temperature);
     }
 }
