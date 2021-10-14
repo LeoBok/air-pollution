fb679f2c379b5ba819d9352f69e264c6ce9dbd8e
Leonardo <leonardoboccali19@gmail.com>
2021-10-14 16:13:33 +0200

    imported elements from dom.js file

---
+++ src/javascript/search_city.js
@@ -0,0 +1,1 @@
+import { container, infoContainer, errParagraph, cityInput, apiKey, cityName, temperature, airQuality, AQIcolor, airborneParticulate25, cityNameValue, temperatureValue, pm25Value } from "./dom.js";
